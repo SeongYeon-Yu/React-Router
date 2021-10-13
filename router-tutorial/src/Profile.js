b1efabb5b909f0044c837a5f572719ffcf1655d2
seongyeon <ysy2565@naver.com>
2021-10-13 21:35:33 +0900

    WithRouter

--- router-tutorial/src/Profile.js
+++ router-tutorial/src/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+import WithRouterSample from "./WithRouterSample";
 
 const data = {
   velopert : {
@@ -14,7 +16,7 @@ const data = {
 const Profile = ({match})=>{
   const {username} = match.params;
   const profile = data[username];
-  if(!profile){
+  if (!profile){
     return <div>존재하지 않는 사용자입니다.</div>
   }
   return (
@@ -23,7 +25,8 @@ const Profile = ({match})=>{
         {username}({profile.name})
       </h3>
       <p>{profile.description}</p>
+      <WithRouterSample/>
     </div>
   )
 }
-export default Profile;
+export default withRouter(Profile);
--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import { Link, Route } from 'react-router-dom';
-import Profile from './Profile';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Profile from "./Profile";
+import WithRouterSample from "./WithRouterSample";
 
-const Profiles = () => {
-    return (
-        <div>
-            <h3>사용자 목록:</h3>
-            <ul>
-                <li>
-                    <Link to="/profiles/youme">김너나 프로필</Link>
-                </li>
-                <li>
-                    <Link to="/profiles/hwihyeol">반휘혈 프로필</Link>
-                </li>
-            </ul>
-
-            <Route 
-                path="/profiles"
-                exact // exact={true} 와 같은 의미
-                render={() => <div>사용자를 선택해 주세요.</div>} // 컴포넌트 자체를 전달하는것이 아니라, 보여 주고 싶은 JSX를 넣음
-                />
-            <Route path="/profiles/:username" component={Profile} />
-        </div>
-    
-    )
-}
-
+const Profiles = ({ match }) => {
+  return (
+    <div>
+      <h3>사용자 목록:</h3>
+      <ul>
+        <li>
+          <Link to="/profiles/velopert">velopert</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong</Link>
+        </li>
+      </ul>
+      <Route
+        path="/profiles"
+        exact
+        render={() => <div>사용자를 선택해주세요.</div>}
+      />
+      <Route path="profiles/:username" component={Profile} />
+      <WithRouterSample/>
+    </div>
+  );
+};
 export default Profiles;
